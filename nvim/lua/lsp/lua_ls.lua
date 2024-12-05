26b909deded61b305265f9d5dc82fd1977a16373
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-04 21:47:29 -0800

    set `vim` global in lua language server

--- nvim/lua/lsp/lua_ls.lua
+++ nvim/lua/lsp/lua_ls.lua
@@ -1,6 +1,20 @@
 local lspc = require("lspconfig")
 
 lspc.lua_ls.setup {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- make Neovim runtime files available to the server
+				library = vim.api.vim_get_runtime_file('', true),
+				-- Prevent prompts for third-party libraries
+				checkThirdParty = false,
+			},
+			telemetry = { enable = false, }, -- don't track me
+		}
+	},
 	on_attach = function()
 		-- Format on save
 		vim.api.nvim_create_autocmd("BufWritePre", {
