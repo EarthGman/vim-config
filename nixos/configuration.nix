7e99f4c2b0991dd1292aa9a69cda23bd34f883a0
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-12-12 19:57:07 -0600

    trying this funky option out

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,20 +1,31 @@
 { pkgs, ... }:
 {
   customRC = "luafile ${./init.lua}";
-  packages.myPlugins = {
-    start = with pkgs.vimPlugins; [
-      plenary-nvim
-      telescope-nvim
-      nvim-treesitter
-      nvim-autopairs
-      lspkind-nvim
-      nvim-lspconfig
-      harpoon
-      undotree
-      vim-fugitive
-    ] ++ (with pkgs.vimPlugins.nvim-treesitter-parsers; [
-      nix
-      lua
-    ]);
+  packages = {
+    environment = {
+      start = with pkgs; [
+        nil
+        lua-language-server
+        nixpkgs-fmt
+        stylua
+      ];
+    };
+
+    plugins = {
+      start = with pkgs.vimPlugins; [
+        plenary-nvim
+        telescope-nvim
+        nvim-treesitter
+        nvim-autopairs
+        lspkind-nvim
+        nvim-lspconfig
+        harpoon
+        undotree
+        vim-fugitive
+      ] ++ (with pkgs.vimPlugins.nvim-treesitter-parsers; [
+        nix
+        lua
+      ]);
+    };
   };
 }
