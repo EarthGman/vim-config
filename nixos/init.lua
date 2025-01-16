2acb730ca40b5c463a831f9dc15e497f695f6357
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-01-15 18:54:18 -0600

    remove duplicate blink.cmp

--- nixos/init.lua
+++ nixos/init.lua
@@ -4,7 +4,6 @@ require("plugins.undotree")
 require("plugins.blink-cmp")
 require("plugins.alpha")
 require("plugins.autopairs")
-require("plugins.blink-cmp")
 
 require("lsp.nil")
 require("lsp.lua_ls")
