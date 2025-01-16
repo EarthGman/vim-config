4ad01184f3752acb1cb9879d90228207b6c155e7
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-01-15 19:28:57 -0600

    add whichkey

--- nixos/init.lua
+++ nixos/init.lua
@@ -3,6 +3,7 @@ require("core.options")
 require("plugins.blink-cmp")
 require("plugins.alpha")
 require("plugins.autopairs")
+require("plugins.which-key")
 
 require("lsp.nil")
 require("lsp.lua_ls")
