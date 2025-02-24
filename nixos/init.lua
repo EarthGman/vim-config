622172500f5e9c7e81779119ad45bf6fcaf19762
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-02-23 20:08:47 -0600

    update init.lua

--- nixos/init.lua
+++ nixos/init.lua
@@ -1,11 +1,8 @@
 require("core.options")
-
 require("plugins.blink-cmp")
-require("plugins.alpha")
 require("plugins.autopairs")
 require("plugins.whichkey")
+require("plugins.snacks")
 
 require("lsp.nil")
 require("lsp.lua_ls")
-
-require("themes.vague")
