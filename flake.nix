e895994aa7fab87a80c278a50b008bd03538b7cc
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-02-28 11:58:46 -0600

    add default app

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
           ];
         };
         packages.default = pkgs.callPackage ./neovim.nix { inherit pkgs; };
+        apps.default = pkgs.callPackage ./neovim.nix { inherit pkgs; };
         devShells.default = pkgs.mkShell {
           shellHook = ''
             ${outputs.packages.${system}.default}/bin/nvim
